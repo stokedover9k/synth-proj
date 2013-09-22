20983f8ea1e6bc3684ac304fcea39d1d137de2bd
Yuriy Skobov <stokedover9k@gmail.com>
2013-09-21 22:13:38 -0400

    Extracted LivingSpec functionality into an abstract class

--- src/test/scala/synth/HarmonicSeriesSpec.scala
+++ src/test/scala/synth/HarmonicSeriesSpec.scala
@@ -1,100 +1,79 @@
 package synth
 
-import org.specs2.mutable._
 import org.specs2.specification.Scope
-import java.io.FileReader
-import au.com.bytecode.opencsv.CSVReader
+import util.LivingSpec
 
+class HarmonicSeriesLivingSpec extends LivingSpec {
 
+  def livingSpecFilename: String = "src/test/resources/harmonic_series_sheet_phase1.csv"
 
-class HarmonicSeriesSpec extends Specification {
+  val dataStartRow: Int = 2
+  val dataStartCol: Int = 1
+  val dataRows: Int = 33
+  val dataCols: Int = 9
+
+  lazy val fundamental = livingSpecData(dataStartRow)(dataStartCol + dataCols - 1).toFloat
 
   "Harmonic Series" should {
 
     trait Fixture extends Scope {
-      val livingSpecFileName = "src/test/resources/harmonic_series_sheet_phase1.csv"
-      val lineStart = 3
-      val colStart = 2
-      val numLines = 33
-      val numCols = 9
-      lazy val fundamental = data(lineStart - 1)(colStart + numCols - 2).toFloat
 
       lazy val series = HarmonicSeries(fundamental)
 
-      lazy val reader = new CSVReader(new FileReader(livingSpecFileName))
-
-      def allNotes = 1 to numLines map { series(_) }
-
-      lazy val data: Vector[Array[String]] = {
-        var dt = Vector[Array[String]]()
-        var row: Array[String] = null
-        while( {row = reader.readNext(); row} != null ) dt = dt :+ row
-        dt
-      }
-
-      def checkColumn[T]( col: Seq[T], colNum: Int ): Unit = {
-        val dt = data.drop(lineStart-1).map { row => row(colNum - 1) }
-        compareCols(col map { _.toString }, dt)
-      }
-
-      def checkQuotedColumn[T]( col: Seq[T], colNum: Int ): Unit = {
-        checkColumn( col map { x => "\"%s\"".format(x.toString) }, colNum )
-      }
-
-      def checkFloatColumn( col: Seq[Float], colNum: Int ): Unit = {
-        val dt = data.drop(lineStart-1).map { row => row(colNum -1).toFloat }
-        compareCols( col, dt )
-      }
-
-      def compareCols[T]( col1: Seq[T], col2: Seq[T] ): Unit =
-        col1 zip col2 foreach { x => x._1 must_== x._2 }
-
-      def asFraction(fraction: (Int, Int)): String = fraction match { case (x, y) => "\"" + x +"/" + y + '"' }
+      lazy val allNotes = 1 to dataRows map { series(_) }
     }
 
     "compute correct set of indices (col 2)" in new Fixture {
       val indices = allNotes map { x => x.degree }
-      checkColumn(indices, 2)
+      mustEqualCol(indices, 1)
     }
 
     "compute correct frequency ratios to fundamental (col 3)" in new Fixture {
       val ratioToFundamental = allNotes map { _.hzSimpleRatio }
-      checkQuotedColumn(ratioToFundamental, 3)
+      mustEqualCol(quotedSeq(ratioToFundamental), 2)
     }
 
     "compute correct frequencies (col 4)" in new Fixture {
-      val frequencies = allNotes map { _.hzSimple } map { f => if(f == f.toInt) f.toInt.toString else f.toString }
-      checkColumn(frequencies, 4)
+      val frequencies = allNotes map {
+        _.hzSimple
+      } map {      // remap so that decimal places show up only if not .0
+        f => if (f == f.toInt) f.toInt.toString else f.toString
+      }
+      mustEqualCol(frequencies, 3)
     }
 
     "compute correct octaves (col 5)" in new Fixture {
       val octaves = allNotes map { _.octave }
-      checkColumn(octaves, 5)
+      mustEqualCol(octaves, 4)
     }
 
     "compute correct octave adjustment factors (col 6)" in new Fixture {
       val adj = allNotes map { _.adjustFactor }
-      checkColumn(adj, 6)
+      mustEqualCol(adj, 5)
     }
 
     "compute correct adjusted frequency ratios to fundamental (col 7)" in new Fixture {
       val adjFactors = allNotes map { _.hzAdjustedRatio }
-      checkQuotedColumn(adjFactors, 7)
+      mustEqualCol(quotedSeq(adjFactors), 6)
     }
 
     "compute correct adjusted and reduced frequency ratios to the fundamental (col 8)" in new Fixture {
       val adjReduced = allNotes map { _.hzReducedRatio }
-      checkQuotedColumn(adjReduced, 8)
+     mustEqualCol(quotedSeq(adjReduced), 7)
     }
 
     "compute correct adjusted and reduced frequency ratios to the fundamental in decimal format (col 9)" in new Fixture {
       val adjReduced = allNotes map { _.hzDecimalRatio }
-      checkFloatColumn(adjReduced, 9)
+      mustEqualMappedCol(_.toFloat)(adjReduced, 8)
     }
 
     "compute correct frequencies within the octave (col 10)" in new Fixture {
-      val frequencies = allNotes map { _.hz } map { f => Math.round(f * 100) / 100f }
-      checkFloatColumn(frequencies, 10)
+      val frequencies = allNotes map {
+        _.hz
+      } map {   // remap to two decimal places of precision
+        f => Math.round(f * 100) / 100f
+      }
+      mustEqualMappedCol(_.toFloat)(frequencies, 9)
     }
   }
 }
---
+++ src/test/scala/util/LivingSpec.scala
@@ -0,0 +1,77 @@
+package util
+
+import org.specs2.mutable.Specification
+import au.com.bytecode.opencsv.CSVReader
+import java.io.FileReader
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: stoked
+ * Date: 9/21/13
+ * Time: 10:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+abstract class LivingSpec extends Specification {
+
+  def livingSpecFilename: String
+
+  val dataStartRow: Int
+  val dataStartCol: Int
+  val dataRows: Int
+  val dataCols: Int
+
+  lazy val livingSpecReader = new CSVReader(new FileReader(livingSpecFilename))
+
+  lazy val livingSpecData: Vector[Array[String]] = {
+    var dt = Vector[Array[String]]()
+    var row: Array[String] = null
+    while( {row = livingSpecReader.readNext(); row} != null ) dt = dt :+ row
+    dt
+  }
+
+  def seqsMustEqual[T](col1: Seq[T], col2: Seq[T]): Unit =
+    col1 zip col2 foreach {
+      case (a, b) => a must_== b
+    }
+
+  def getDataCol(colIndex: Int): Seq[String] = {
+    livingSpecData.drop(dataStartRow) map {
+      row => row(colIndex)
+    }
+  }
+
+  def getDataRow(rowIndex: Int): Seq[String] = {
+    livingSpecData.drop(dataStartRow)(rowIndex).drop(dataStartCol)
+  }
+
+  def mustEqualCol[T](col: Seq[T], colNum: Int): Unit = {
+    seqsMustEqual(col map {
+      _.toString
+    }, getDataCol(colNum))
+  }
+
+  def mustEqualMappedCol[T](f: String => T)(col: Seq[T], colNum: Int): Unit =
+    seqsMustEqual(col, getDataCol(colNum) map f)
+
+  def mustEqualMappedRow[T](f: String => T)(row: Seq[T], rowNum: Int): Unit =
+    seqsMustEqual(row, getDataRow(rowNum) map f)
+
+  def mustEqualRow[T](row: Seq[T], rowNum: Int): Unit = {
+    seqsMustEqual(row map {
+      _.toString
+    }, getDataRow(rowNum))
+  }
+
+  def mustEqualFloatCol(col: Seq[Float], colNum: Int): Unit = {
+    val dt = livingSpecData.drop(dataStartRow) map {
+      row => row(colNum).toFloat
+    }
+    seqsMustEqual(col, dt)
+  }
+
+  def quotedSeq[T](seq: Seq[T]): Seq[String] = seq map { s => "\"%s\"".format(s.toString) }
+
+  def floatSeq(seq: Seq[String]): Seq[Float] = seq map { _.toFloat }
+
+}
