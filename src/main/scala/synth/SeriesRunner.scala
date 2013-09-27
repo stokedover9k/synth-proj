70c10526c9dc30d9768737171c956a07f415128b
Yuriy Skobov <stokedover9k@gmail.com>
2013-09-27 12:44:26 -0400

    cleaned runner

--- src/main/scala/synth/SeriesRunner.scala
+++ src/main/scala/synth/SeriesRunner.scala
@@ -1,59 +1,4 @@
-//package synth
-//
-///**
-// * Created with IntelliJ IDEA.
-// * User: yuriy
-// * Date: 9/16/13
-// * Time: 9:30 PM
-// * To change this template use File | Settings | File Templates.
-// */
-//object HarmonicSeriesRunner {
-//
-//  val usage = "[USAGE]: scala synth.HarmonicSeriesMain <fundamental>\n" +
-//              "         where 'fundamental' is leftOp floating point valued fundamental frequency."
-//
-//  def main(args: Array[String]) = {
-//
-//    val fundamental = try {
-//      args(0).toFloat
-//    } catch {
-//      case _: Throwable => {
-//        System.err.println("[Error]: expecting fundamental frequency as argument\n" + usage)
-//        sys.exit(1)
-//      }
-//    }
-//
-//    val series = HarmonicSeries(fundamental)
-//    def asFraction(x: (Int, Int)): String = x match { case (x, y) => x + "/" + y }
-//
-//    print(
-//      "-----+-------+---------+-----+-----+-------+-------+---------+-----------\n" +
-//      " deg-| ratio |  freq-  | oct-| fac-| adju- |  red- | reduced | frequency \n" +
-//      " ree |       |  uency  | ave | tor | sted  |  uced | decimal | in octave \n" +
-//      "-----+-------+---------+-----+-----+-------+-------+---------+-----------\n" )
-//
-//    1 to 32 map { x => series(x) } foreach( note => {
-//
-//      val str = "%4d %7s %9.2f %5d %5d %7s %7s %9.4f %9.2f".format(
-//      note.degree
-//      , note.hzSimpleRatio
-//      , note.hzSimple
-//      , note.octave
-//      , note.adjustFactor
-//      , note.hzAdjustedRatio
-//      , note.hzReducedRatio
-//      , note.hzDecimalRatio
-//      , note.hz
-//      )
-//
-//      println(str)
-//      })
-//  }
-//}
-
 package synth
-
-import util.expr.Expr
 
 /**
 * Created with IntelliJ IDEA.
@@ -62,6 +7,7 @@ import util.expr.Expr
 * Time: 9:30 PM
 * To change this template use File | Settings | File Templates.
 */
+
 object SeriesRunner {
 
   val usage = "[USAGE]: scala synth.SeriesMain <series:[hp]> <fundamental>\n" +
