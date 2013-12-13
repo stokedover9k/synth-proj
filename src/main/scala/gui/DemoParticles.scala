7fb3e2a24f7929a7170c5b633049b8ae20af5acc
Yuriy <stokedover9k@gmail.com>
2013-12-13 12:17:22 -0500

    added demo for modulating scales

--- src/main/scala/gui/DemoParticles.scala
+++ src/main/scala/gui/DemoParticles.scala
@@ -3,7 +3,7 @@ package gui
 import scala.swing._
 import java.awt.Color
 import synth.scales._
-import synth.models.Particles.{DES, HeatedParticleSystemProc, NoteEvent}
+import synth.models.Particles.{HeatedAllScaleParticleProc, DES, HeatedParticleSystemProc, NoteEvent}
 import synth.models.Particles.DesNoteEventTester.NoteLifePrinter
 import scala.collection.immutable.SortedSet
 import synth.models.DemoChordPlayer
@@ -19,7 +19,7 @@ import synth.models.Particles.NoteEvent.Birth
  */
 
 class ScalePicker(fundamental: Float) extends BoxPanel(Orientation.Vertical) {
-  private val scales = Map[String, () => TypedScale](
+  protected lazy val scales = Map[String, () => TypedScale](
     "Full (Pythagorean)" -> {
       () => ScaleBuilderPythagFull(fundamental).build
     },
@@ -65,6 +65,24 @@ class ScalePicker(fundamental: Float) extends BoxPanel(Orientation.Vertical) {
   reactions += {
     case ButtonClicked(button) => scale = getScale(button.text)
   }
+}
+
+class FullScalePicker(fundamental: Float) extends ScalePicker(fundamental) {
+
+  override protected lazy val scales = Map[String, () => TypedScale](
+    "Full (Pythagorean)" -> {
+      () => ScaleBuilderPythagFull(fundamental).build
+    },
+    "Full (Even Temperment)" -> {
+      () => ScaleBuilderEvenTempFull(fundamental).build
+    },
+    "Chromatic (Ptolemy)" -> {
+      () => ScaleBuilderPtolemyChromatic(fundamental).build
+    },
+    "Mean Tone" -> {
+      () => ScaleBuilderMeanTone(fundamental).build
+    }
+  )
 }
 
 object DemoParticles extends SimpleSwingApplication {
@@ -145,6 +163,104 @@ object DemoParticles extends SimpleSwingApplication {
   }
 
   val scalePicker = new ScalePicker(528f)
+
+  val songDisplay = new ScrollPane {
+    contents = new SongDisplay(song)
+    preferredSize = new Dimension(800, 300)
+  }
+
+  def top: Frame = new MainFrame {
+    title = "Particles Demo"
+    contents = new BorderPanel {
+      add(playButton, West)
+      add(scalePicker, Center)
+      add(songDisplay, South)
+    }
+  }
+}
+
+object DemoModulatingParticles extends SimpleSwingApplication {
+
+  class NoteLabel(note: String, duration: Float) extends Button(duration.toString) {
+    enabled = false
+    preferredSize = new Dimension((duration * 70).toInt, 30)
+    background = Color.LIGHT_GRAY
+    font = new Font("Monospaced", 0, 10)
+
+    override def paintComponent(g: swing.Graphics2D) = {
+      super.paintComponent(g)
+      g.setColor(Color.BLACK)
+      g.drawString(note, 5, 10)
+    }
+  }
+
+  class ScaleDisplay(name: String, scale: TypedScale) extends FlowPanel {
+    val notes = scale.allNames map (new NoteLabel(_, 1))
+    contents ++= notes
+  }
+
+  class ChordDisplay(chord: Seq[NoteEvent.Note], duration: Float) extends BoxPanel(Orientation.Vertical) {
+    preferredSize = new Dimension((duration * 70).toInt, 100)
+    contents += new Label(duration.toString)
+    contents ++= (chord map (note => new NoteLabel(note, duration)))
+  }
+
+  class SongDisplay(song: List[(Seq[NoteEvent.Note], Int)]) extends FlowPanel {
+    contents ++= song map {
+      case (chord, duration) => new ChordDisplay(chord, duration * .5f)
+    }
+  }
+
+  //-----------------------------------
+
+  val systemProcessor = new HeatedAllScaleParticleProc
+
+  implicit val proc = NoteLifePrinter andThen systemProcessor
+
+  implicit val EOrder: Ordering[NoteEvent] =
+    Ordering[(Int, Int, Option[NoteEvent.Note])].on[NoteEvent](e => (e.time, e.priority, e.note))
+
+  val des = {
+    val notes = ScaleBuilderRameau(528f).build.allNames
+
+    (1 to 10).foldLeft(new DES[NoteEvent](SortedSet())) {
+      case (d, i) => {
+        val note = notes((Math.random() * notes.size).toInt)
+        val bornAt = (Math.random() * 10).toInt
+        val lifespan = (Math.random() * 10).toInt
+        d addEvent Birth(Option(note), bornAt, lifespan)
+      }
+    }
+  }
+
+  des.play
+
+  val song = systemProcessor.songBuffer.toList.map {
+    case (a, b) => (a.toSeq, b)
+  }
+
+  import BorderPanel.Position._
+
+  val playButton = new Button("PLAY")
+
+  listenTo(playButton)
+
+  reactions += {
+    case ButtonClicked(button) => {
+      if (button == playButton) {
+        button.enabled = false
+        try {
+          DemoChordPlayer.demoPlayChordsSeconds(scalePicker.scale, song)
+        } catch {
+          case _: Throwable => System.err.println("could not play the song in this scale")
+        }
+        button.enabled = true
+      }
+    }
+    case _ =>
+  }
+
+  val scalePicker = new FullScalePicker(528f)
 
   val songDisplay = new ScrollPane {
     contents = new SongDisplay(song)
